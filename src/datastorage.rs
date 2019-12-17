65930cc14601437ed646df6955cb3d0e3f67ba37
makeev_kp <kmakeev@arbitr.ru>
2019-12-17 18:06:38 +0300

    first result

--- src/datastorage.rs
+++ src/datastorage.rs
@@ -1,36 +1,61 @@
 extern crate pyo3;
+extern crate rand;
 use pyo3::prelude::*;
-// use pyo3::wrap_function;
+use pyo3::exceptions;
 use pyo3::types::{PyDict, PyTuple};
+use std::cmp;
+use rand::Rng;
+
 
 
 #[pyclass(module= "datastorage_lib")]
 pub struct DataStorage {
-    pub size: usize,
+    #[pyo3(get)]
+    pub size: u32,
+    #[pyo3(get)]
     pub is_visual: bool,
-    pub frame_height: u32,
-    pub frame_width: u32,
-    pub agent_history_length: u32,
-    pub batch_size: u32,
-    pub count: u32,
-    pub current: u32,
+    #[pyo3(get)]
+    pub frame_height: usize,
+    #[pyo3(get)]
+    pub frame_width: usize,
+    #[pyo3(get)]
+    pub agent_history_length: usize,
+    #[pyo3(get)]
+    pub batch_size: usize,
+    #[pyo3(get)]
+    pub count: usize,
+    #[pyo3(get)]
+    pub current: usize,
+    #[pyo3(get)]
     pub actions: Vec<i32>,
+    #[pyo3(get)]
     pub rewards: Vec<i32>,
-    pub frames: Vec<i8>,
+    #[pyo3(get)]
+    pub frames: Vec<Vec<Vec<u8>>>,
+    #[pyo3(get)]
     pub terminal_flags: Vec<bool>,
+    #[pyo3(get)]
+    pub indices: Vec<usize>,
+    #[pyo3(get)]
+    pub states: Vec<Vec<Vec<Vec<u8>>>>,
+    #[pyo3(get)]
+    pub new_states: Vec<Vec<Vec<Vec<u8>>>>,
 }
 
 #[pymethods]
 impl DataStorage {
     #[new]
-    #[args(size=1000000, is_visual=true, frame_height=84, frame_width=84, agent_history_length=4, batch_size=4, kwargs="**")]
-    pub fn new(obj: &PyRawObject, size: usize, is_visual: bool, frame_height: u32, frame_width: u32, agent_history_length: u32, batch_size: u32, kwargs: Option<&PyDict>){
+    #[args(size=100000, is_visual=true, frame_height=84, frame_width=84, agent_history_length=4, batch_size=4, kwargs="**")]
+    pub fn new(obj: &PyRawObject, size: usize, is_visual: bool, frame_height: usize, frame_width: usize, agent_history_length: usize, batch_size: usize, kwargs: Option<&PyDict>){
         let mut actions = vec![0; size];
         let mut rewards = vec![0; size];
-        let mut frames = vec![0; (size as u32 * frame_width * frame_height) as usize];
-
+        let mut frames = vec![vec![vec![0; frame_width as usize]; frame_height as usize]; size];
         let mut terminal_flags = vec![false; size];
-        obj.init(DataStorage{size: size,
+        let mut indices = vec![0; batch_size];
+        let mut states = vec![vec![vec![vec![0; frame_width as usize]; frame_height as usize]; agent_history_length]; batch_size];
+        let mut new_states = vec![vec![vec![vec![0; frame_width as usize]; frame_height as usize]; agent_history_length]; batch_size];
+        obj.init(DataStorage{
+            size: size as u32,
             is_visual: is_visual,
             frame_height: frame_height,
             frame_width: frame_width,
@@ -42,7 +67,88 @@ impl DataStorage {
             rewards: rewards,
             frames: frames,
             terminal_flags: terminal_flags,
+            indices: indices,
+            states: states,
+            new_states: new_states,
         });
+    }
+
+    fn add_experience(& mut self, action: i32, frame: Vec<Vec<u8>>, reward: i32, terminal: bool) -> PyResult<()> {
+        let msg = format!("expected frames shape {:?}, found {:?}", (self.frames[0].len(), self.frames[0][0].len()),
+                          (frame.len(), frame[0].len()));
+        if frame.len() != self.frame_height {
+            Err(exceptions::ValueError::py_err(msg))
+        } else if frame[0].len() != self.frame_width  {
+            Err(exceptions::ValueError::py_err(msg))
+        } else {
+            self.actions[self.current] = action;
+            self.frames[self.current] = frame;
+            self.rewards[self.current] = reward;
+            self.terminal_flags[self.current] = terminal;
+            self.count = cmp::max(self.count, self.current +1);
+            self.current = (self.current + 1) % self.size as usize;
+            Ok(())
+        }
+    }
+
+    fn _get_state(& self, index: usize) -> PyResult<Vec<Vec<Vec<u8>>>> {
+        if self.count == 0 {
+            Err(exceptions::ValueError::py_err("The storage data is empty"))
+        } else if index < self.agent_history_length - 1 {
+            Err(exceptions::ValueError::py_err(format!("Index must be min {}", self.agent_history_length -1)))
+        } else {
+            let frame = & self.frames[index - self.agent_history_length+1..index+1];
+            Ok(frame.to_vec())
+        }
+    }
+
+    fn _get_valid_indices(& mut self) {
+        for i in 0..self.batch_size {
+            let mut index: usize = 0;
+            while true {
+                index = rand::thread_rng().gen_range(self.agent_history_length, self.count -1);
+                if index < self.agent_history_length {
+                    continue;
+                }
+                if (index >= self.current) && ((index - self.agent_history_length) <= self.current) {
+                    continue;
+                }
+                if self.terminal_flags[index - self.agent_history_length..index].contains(&true) {
+                    continue;
+                }
+                break;
+            }
+            self.indices[i] = index;
+        }
+    }
+
+    fn get_minibatch(& mut self) -> PyResult<Vec<Vec<Vec<Vec<u8>>>>> {
+        if self.count < self.agent_history_length {
+            Err(exceptions::ValueError::py_err("Not enough data in the storage to get a minibatch"))
+        } else {
+            self._get_valid_indices();
+            for (i, idx) in self.indices.iter().enumerate() {
+
+                match self._get_state(*idx-1) {
+                    Err(err) => {
+                        println!("Error retrieving data in the storage to get a minibatch");
+                    },
+                    Ok(t) => {
+                        self.states[i] = t;
+                    }
+                }
+                match self._get_state(*idx) {
+                    Err(err) => {
+                        println!("Error retrieving data in the storage to get a minibatch");
+                    },
+                    Ok(t) => {
+                        self.new_states[i] = t;
+                    }
+                }
+            }
+
+            Ok(self.states.to_vec())
+        }
     }
 }
 
---
+++ tests/test_datastoragelib.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import pytest
+from datastoragelib import DataStorage
+
+
+class TestDatastoragelib:
+
+    def test_new_datastorage(self):
+        ds = DataStorage(size=100, frame_height=8, frame_width=4, agent_history_length=5, batch_size=32)
+        assert (ds.count == 0)
+        assert (ds.current == 0)
+        assert (ds.frame_height == 8)
+        assert (ds.frame_width == 4)
+        assert (len(ds.actions) == 100)
+        assert (len(ds.rewards) == 100)
+        assert (len(ds.terminal_flags) == 100)
+        assert (len(ds.frames) == 100)
+        assert (len(ds.frames[0]) == 8)
+        assert (len(ds.frames[0][0]) == 4)
+        assert (len(ds.states) == 32)
+        assert (len(ds.states[0]) == 5)
+        assert (len(ds.states[0][0]) == 8)
+        assert (len(ds.states[0][0][0]) == 4)
+        assert (len(ds.new_states) == 32)
+        assert (len(ds.new_states[0]) == 5)
+        assert (len(ds.new_states[0][0]) == 8)
+        assert (len(ds.new_states[0][0][0]) == 4)
+
+    def test_add_experience(self):
+        ds = DataStorage(size=10, frame_height=5, frame_width=2)
+        list = [[255 for x in range(2)] for y in range(5)]
+        ds.add_experience(1, list, 0, True)
+        assert (ds.count == 1)
+        assert (ds.current == 1)
+        assert (ds.frames[ds.current - 1] == list)
+        list = [[255 for x in range(3)] for y in range(5)]
+        try:
+            ds.add_experience(1, list, 0, True)
+            assert False
+        except ValueError:
+            assert True
+        list = [[256 for x in range(3)] for y in range(5)]
+        try:
+            ds.add_experience(1, list, 0, True)
+            assert False
+        except OverflowError:
+            assert True
+
+    def test_get_state(self):
+        ds = DataStorage(size=10, frame_height=5, frame_width=2, agent_history_length=4)
+        list = [[255 for x in range(2)] for y in range(5)]
+        for i in range(10):
+            ds.add_experience(1, list, 1, True)
+        state = ds._get_state(4)
+        assert (len(state) == 4)
+
+    def test_get_valid_indices(self):
+        ds = DataStorage(size=1000, frame_height=5, frame_width=2, agent_history_length=4)
+        list = [[255 for x in range(2)] for y in range(5)]
+        for i in range(100):
+            ds.add_experience(1, list, 1, False)
+        assert (len(ds.indices) == 4)
+        assert (ds.indices[0] == 0)
+        ds._get_valid_indices()
+        assert (ds.indices[0] != 0)
